feb7dac754f0388784bb0331b8bcd6ea66068eae
DaProclaima <sn.itpro@gmail.com>
2020-01-05 19:20:34 +0100

    feat: adds select level and pokemon card fetched

--- exo2/lib/searchEngine.js
+++ exo2/lib/searchEngine.js
@@ -2,11 +2,53 @@ $(document).ready(function () {
 
   let api_url = 'https://pokeapi.co/api/v2/';
 
+  function generateLvSelectEl(el){
+    let selectEl='<form><select name="niveau" id="niveau">'+
+      '<option value="">Sélectionner niveau 0-100</option>';
+
+    for(let i=1; i<=100; i++){
+      selectEl+='<option value="'+i+'">Niveau '+ i + '</option>';
+    }
+
+    selectEl+= '</select></form>';
+
+    $(el).append(selectEl);
+  }
+
+
+
+
+
+
+
+  function calculateStat(data, lv){
+  //  selectLv * data.truc * data.truc
+  }
+
+  function render(response){
+    // let data = JSON.stringify(response);
+    let data = response;
+    // console.log(data);
+    let pokemonCard = '<div class="card-pokemon" id="card-' +data.id+'">'
+      +'<div>'
+        +'<div><span class="name">Nom: '+data.name+ '</span></div>'
+        +'<div class="image"><img src="'+data.sprites.front_default+ '"></div>'
+        +'<div><span class="weight">Poid: '+data.weight+ 'kg</span></div>'
+        +'<div><span class="types">Type: '+data.types[0].type.name+ '</span></div>'
+        +'<div class="select-level"></div>'
+      +'</div>';
+
+
+    $('#list-card').append(pokemonCard);
+    generateLvSelectEl('.select-level');
+  }
+
+
   $('#search-btn').click(function (e) {
     e.preventDefault();
     let query = $('#query').val();
     console.log("query vaut "+ query);
-    let url = api_url + 'pokemon-species/' + query+'/';
+    let url = api_url + 'pokemon/' + query+'/';
     let settings = {
       "async": true,
       "crossDomain": true,
@@ -14,6 +56,7 @@ $(document).ready(function () {
       "url": url,
       "success" : function (response) {
         console.log(response);
+        render(response);
       },
     };
     $.ajax(settings)
